3220257a586a8160b5a8d5cafc4cad80bbaa5e74
Arekusei Kirigaya <kshigetora@mail.ru>
2022-10-18 00:46:39 +0700

    Added [] operator

--- Sparse_Matrix.cpp
+++ Sparse_Matrix.cpp
@@ -1,9 +1,12 @@
 #include "Sparse_Matrix.h"
 
+#include <iostream>
+
 namespace SMatrix {
     SparseMatrix::SparseMatrix() {
         size = 0;
         row_val.resize(1, 0);
+        nnz = 0;
     }
 
     SparseMatrix::SparseMatrix(const std::vector<double>& val, size_t size,
@@ -18,10 +21,38 @@ namespace SMatrix {
         this->val = val;
         this->column_val = column_val;
         this->row_val = row_val;
+        nnz = val.size();
+    }
+
+    size_t SparseMatrix::getSize() const {
+        return this->size;
+    }
+
+    SparseMatrix::SparseMatrixLine SparseMatrix::operator[] (size_t row) const {
+        return {row, *this};
+    }
+
+    double SparseMatrix::SparseMatrixLine::operator[] (size_t column) const {
+        size_t upper_bound = row < (matrix.row_val.size() - 1) ? matrix.row_val[row + 1] : matrix.val.size();
+        for (size_t i = matrix.row_val[row]; i < upper_bound; ++i) {
+            if (matrix.column_val[i] == column)
+                return matrix.val[i];
+        }
+        return 0.f;
     }
 }
 
 int main (int argc, char* argv[]) {
-    SMatrix::SparseMatrix A({1, 2, 3}, 3, {0, 1, 2}, {0, 1, 2, 3});
+    using namespace std;
+    vector<double> a = {1, 2, 3};
+    vector<size_t> b = {0, 1, 2};
+    vector<size_t> c = {0, 1, 2, 3};
+    SMatrix::SparseMatrix A(a, 3, b, c);
+    for (size_t i = 0; i < A.getSize(); ++i) {
+        for (size_t j = 0; j < A.getSize(); ++j) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
--- Sparse_Matrix.h
+++ Sparse_Matrix.h
@@ -8,10 +8,25 @@ class SparseMatrix {
     std::vector<size_t> column_val;
     std::vector<size_t> row_val;
     size_t size;
+    size_t nnz; // number of non-zero elements
+
+    struct SparseMatrixLine {
+        size_t row = 0;
+        const SparseMatrix& matrix;
+        double operator[] (size_t column) const;
+    };
 public:
     SparseMatrix();
     SparseMatrix(const std::vector<double>& val, size_t size,
         const std::vector<size_t>& column_val, const std::vector<size_t>& row_val);
+
+    size_t getSize() const;
+    
+    SparseMatrix::SparseMatrixLine operator[] (size_t row) const;
+
+    std::vector<double> operator* (const std::vector<double>& v) const;
+
+    std::vector<double> SolveSystem(std::vector<double> f);
 };
 
 }
